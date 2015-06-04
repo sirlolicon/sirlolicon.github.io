447414c8c113feae9b53fe01da73b790472e0b13
sirlolicon <dpony13@gmail.com>
2015-06-04 11:23:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 50,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Untitled Dice',
+  app_name: 'Small Bits',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  recaptcha_sitekey: '6LeZ0gcTAAAAAHN9AhFEi3vfKsF4OVsbVS8dU8OX',  // <----- EDIT ME!
+  redirect_uri: 'https://sirlolicon.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: 'https://a-chat-server.herokuapp.com',
